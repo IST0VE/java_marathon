c0defd0d81dac8eaa354a6bbd4738fc7615bc6ad
Nikita Korovko <nikorovko@gmail.com>
2022-01-11 20:52:57 +0400

    added day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,27 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String string1;
+        int x1 = 20000;
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i <= x1; i++){
+            string1 = i + " ";
+            System.out.print(string1);
+            //string1 += i + " ";
+        }
+        //System.out.println(string1);
+        System.out.println();
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime));
 
+        StringBuilder string2 = new StringBuilder();
+        int x2 = 20000;
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i <= x2; i++){
+            string2.append(i).append(" ");
+        }
+        stopTime = System.currentTimeMillis();
+        System.out.println(string2);
+        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime));
     }
 }
